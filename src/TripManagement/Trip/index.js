2d3085cb56b27f1db8b9b3a0adf1ac687c27d5fd
Rich Turnbull <rpturnbull1@gmail.com>
2019-06-19 18:04:14 -0500

    Trip.getByIds

--- src/TripManagement/Trip/index.js
+++ src/TripManagement/Trip/index.js
@@ -2,10 +2,6 @@ var Entity = require('../../Entity')
 var Method = require('../../Method')
 
 module.exports = new Entity('trips', {
-  create: new Method({
-    method: 'POST',
-    path: '/create',
-  }),
   retrieve: new Method({
     method: 'GET',
     path: '/{id}',
@@ -14,19 +10,8 @@ module.exports = new Entity('trips', {
   search: new Method({
     method: 'GET',
   }),
-  updateArrival: new Method({
+  getByIds: new Method({
     method: 'POST',
-    path: '/{id}/updateArrival',
-    urlParams: ['id'],
-  }),
-  updateDeparture: new Method({
-    method: 'POST',
-    path: '/{id}/updateDeparture',
-    urlParams: ['id'],
-  }),
-  updateGuestInfo: new Method({
-    method: 'POST',
-    path: '/{id}/updateGuestInfo',
-    urlParams: ['id'],
+    path: '/getByIds',
   }),
 })
